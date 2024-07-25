502eaaf691db548c5f613ef0f8bab2a71767afe4
tomaswatson <twatson@frba.utn.edu.ar>
2024-07-25 20:23:00 -0300

    arreglo metodo usar y test de tarjetaColaborador

--- src/main/java/org/example/tarjetas/TarjetaColaborador.java
+++ src/main/java/org/example/tarjetas/TarjetaColaborador.java
@@ -11,19 +11,24 @@ import org.example.personas.roles.Rol;
 import org.example.repositorios.RepositorioSolicitudesApertura;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @Getter
 @Setter
 public class TarjetaColaborador extends Tarjeta  {
     private int limiteDeTiempoEnMinutos;
 
+    public TarjetaColaborador(){
+        this.setUsos(new ArrayList<>());
+        this.limiteDeTiempoEnMinutos = 3;
+    }
+
     @Override
     public void usar(Rol duenio, Heladera heladera) throws LimiteDeTiempoSuperado
     {
         RepositorioSolicitudesApertura repositorioSolicitudesApertura = RepositorioSolicitudesApertura.getInstancia();
 
-        SolicitudDeApertura solicitudDeApertura =
-                repositorioSolicitudesApertura.buscarSolicitudDeApertura(heladera,((Colaborador) duenio).getTarjetaColaborador());
+        SolicitudDeApertura solicitudDeApertura = repositorioSolicitudesApertura.buscarSolicitudDeApertura(heladera,this);
         LocalDateTime fechaActual = LocalDateTime.now();
         if(solicitudDeApertura.getFechaCreacion().plusHours(limiteDeTiempoEnMinutos).isBefore(fechaActual)){
             throw new LimiteDeTiempoSuperado();
---
+++ src/test/java/tests/TarjetaColaboradorTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import org.example.colaboraciones.contribuciones.heladeras.Heladera;
+import org.example.excepciones.LimiteDeTiempoSuperado;
+import org.example.personas.roles.Colaborador;
+import org.example.repositorios.RepositorioSolicitudesApertura;
+import org.example.tarjetas.SolicitudDeApertura;
+import org.example.tarjetas.TarjetaColaborador;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDateTime;
+
+public class TarjetaColaboradorTest {
+    private final TarjetaColaborador tarjetaColaborador = new TarjetaColaborador();
+
+    @Mock
+    private Colaborador duenioMock;
+    @Mock
+    private Heladera heladeraMock;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void usarTarjeta() throws LimiteDeTiempoSuperado{
+        RepositorioSolicitudesApertura repositorioSolicitudesApertura = RepositorioSolicitudesApertura.getInstancia();
+        repositorioSolicitudesApertura.agregarSolicitudDeApertura(new SolicitudDeApertura(heladeraMock, LocalDateTime.now(),tarjetaColaborador));
+        tarjetaColaborador.usar(duenioMock, heladeraMock);
+
+        Assertions.assertEquals(1,tarjetaColaborador.getUsos().size());
+    }
+
+    @Test
+    public void testNoSePuedeExcederElLimiteDeTiempo() throws LimiteDeTiempoSuperado{
+        RepositorioSolicitudesApertura repositorioSolicitudesApertura = RepositorioSolicitudesApertura.getInstancia();
+        repositorioSolicitudesApertura.agregarSolicitudDeApertura(new SolicitudDeApertura(heladeraMock, LocalDateTime.now().minusHours(3),tarjetaColaborador));
+
+        Assertions.assertThrows(LimiteDeTiempoSuperado.class, ()->tarjetaColaborador.usar(duenioMock,heladeraMock));
+    }
+}
