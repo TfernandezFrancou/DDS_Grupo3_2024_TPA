9026990f73c8207cd058fae9fff109839a4b3149
tomaswatson <twatson@frba.utn.edu.ar>
2024-07-25 19:27:30 -0300

    metodo usar en tarjetaColaborador

--- src/main/java/org/example/tarjetas/TarjetaColaborador.java
+++ src/main/java/org/example/tarjetas/TarjetaColaborador.java
@@ -3,9 +3,14 @@ package org.example.tarjetas;
 import lombok.Getter;
 import lombok.Setter;
 import org.example.colaboraciones.contribuciones.heladeras.Heladera;
+import org.example.colaboraciones.contribuciones.heladeras.Uso;
+import org.example.excepciones.LimiteDeTiempoSuperado;
 import org.example.personas.Persona;
 import org.example.personas.roles.Colaborador;
 import org.example.personas.roles.Rol;
+import org.example.repositorios.RepositorioSolicitudesApertura;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -13,8 +18,17 @@ public class TarjetaColaborador extends Tarjeta  {
     private int limiteDeTiempoEnMinutos;
 
     @Override
-    public void usar(Rol duenio, Heladera heladera)
+    public void usar(Rol duenio, Heladera heladera) throws LimiteDeTiempoSuperado
     {
-        //TODO
+        RepositorioSolicitudesApertura repositorioSolicitudesApertura = RepositorioSolicitudesApertura.getInstancia();
+
+        SolicitudDeApertura solicitudDeApertura =
+                repositorioSolicitudesApertura.buscarSolicitudDeApertura(heladera,((Colaborador) duenio).getTarjetaColaborador());
+        LocalDateTime fechaActual = LocalDateTime.now();
+        if(solicitudDeApertura.getFechaCreacion().plusHours(limiteDeTiempoEnMinutos).isBefore(fechaActual)){
+            throw new LimiteDeTiempoSuperado();
+        }
+        Uso nuevoUso = new Uso(fechaActual, heladera);
+        this.getUsos().add(nuevoUso);
     }
 }
