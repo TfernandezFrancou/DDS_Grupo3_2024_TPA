830bca2c99eca9bb86ab713f23c5b32a35ee6909
Fausto Fusse <faustofusse@gmail.com>
2024-12-04 20:29:01 -0300

    suscripciones

--- src/main/java/org/example/subscripcionesHeladeras/PublisherViandasDisponibles.java
+++ src/main/java/org/example/subscripcionesHeladeras/PublisherViandasDisponibles.java
@@ -3,6 +3,7 @@ package org.example.subscripcionesHeladeras;
 import lombok.Getter;
 import lombok.Setter;
 import org.example.colaboraciones.contribuciones.heladeras.Heladera;
+import org.example.repositorios.RepoHeladeras;
 
 import javax.mail.MessagingException;
 import java.util.ArrayList;
@@ -11,22 +12,25 @@ import java.util.List;
 @Getter
 @Setter
 public class PublisherViandasDisponibles extends PublisherHeladera {
-    private List<SubscripcionViandasDisponibles> subscripciones;
+    // private List<SubscripcionViandasDisponibles> subscripciones;
 
     public PublisherViandasDisponibles() {
-        this.subscripciones = new ArrayList<>();
+        // this.subscripciones = new ArrayList<>();
     }
 
     @Override
     public void suscribir(SubscripcionHeladera subscripcion) {
-        subscripciones.add((SubscripcionViandasDisponibles) subscripcion);
+        // subscripciones.add((SubscripcionViandasDisponibles) subscripcion);
+        RepoHeladeras.getInstancia().agregarSubscripcion(subscripcion);
     }
 
     @Override
     public void notificarATodos(Heladera heladera) throws MessagingException {
-        for(SubscripcionViandasDisponibles subscripcion : subscripciones){
-            if(heladera.getViandasEnHeladera() == subscripcion.getCantidadDeViandas())
+        List<SubscripcionViandasDisponibles> subscripciones = RepoHeladeras.getInstancia().obtenerSubscripcionesViandasDisponibles(heladera.getIdHeladera());
+        for (SubscripcionViandasDisponibles subscripcion : subscripciones){
+            if (heladera.getViandasEnHeladera() == subscripcion.getCantidadDeViandas()) {
                 subscripcion.notificar(heladera);
+            }
         }
     }
 }
--- src/main/java/org/example/subscripcionesHeladeras/PublisherViandasFaltantes.java
+++ src/main/java/org/example/subscripcionesHeladeras/PublisherViandasFaltantes.java
@@ -3,6 +3,7 @@ package org.example.subscripcionesHeladeras;
 import lombok.Getter;
 import lombok.Setter;
 import org.example.colaboraciones.contribuciones.heladeras.Heladera;
+import org.example.repositorios.RepoHeladeras;
 
 import javax.mail.MessagingException;
 import java.util.ArrayList;
@@ -11,22 +12,25 @@ import java.util.List;
 @Getter
 @Setter
 public class PublisherViandasFaltantes extends PublisherHeladera{
-    private List<SubscripcionViandasFaltantes> subscripciones;
+    // private List<SubscripcionViandasFaltantes> subscripciones;
 
     public PublisherViandasFaltantes() {
-        this.subscripciones = new ArrayList<>();
+        // this.subscripciones = new ArrayList<>();
     }
 
     @Override
     public void suscribir(SubscripcionHeladera subscripcion) {
-        subscripciones.add((SubscripcionViandasFaltantes) subscripcion);
+        // subscripciones.add((SubscripcionViandasFaltantes) subscripcion);
+        RepoHeladeras.getInstancia().agregarSubscripcion(subscripcion);
     }
 
     @Override
     public void notificarATodos(Heladera heladera) throws MessagingException {
-        for(SubscripcionViandasFaltantes subscripcion : subscripciones){
-            if(heladera.faltanteParaLlenar() == subscripcion.getCantidadDeViandas())
+        List<SubscripcionViandasFaltantes> subs = RepoHeladeras.getInstancia().obtenerSubscripcionesViandasFaltantes(heladera.getIdHeladera());
+        for (SubscripcionViandasFaltantes subscripcion : subs) {
+            if (heladera.faltanteParaLlenar() == subscripcion.getCantidadDeViandas()) {
                 subscripcion.notificar(heladera);
+            }
         }
     }
 }
