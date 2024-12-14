f8b291fb40f9ce756ca813993f20968aa4ee0d19
Tomás Fernández Francou <Tfernndezfrancou@frba.utn.edu.ar>
2024-12-13 23:33:57 -0300

    Agrego callback SSO

--- src/main/java/org/example/autenticacion/GoogleAuthController.java
+++ src/main/java/org/example/autenticacion/GoogleAuthController.java
@@ -1,10 +1,12 @@
 package org.example.autenticacion;
 
-import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl;
+import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.oauth2.Oauth2;
+import com.google.api.services.oauth2.model.Userinfo;
 import io.javalin.http.Context;
 import org.example.config.GoogleConfig;
 
@@ -14,7 +16,7 @@ import java.util.Collections;
 
 public class GoogleAuthController {
 
-    private static AuthorizationCodeFlow flow;
+    private static GoogleAuthorizationCodeFlow flow;
 
     static {
         try {
@@ -37,6 +39,32 @@ public class GoogleAuthController {
     }
 
     public static void oauth2callback(Context ctx) {
-        // Implementación del callback
+        String code = ctx.queryParam("code");
+        if (code != null) {
+            try {
+                TokenResponse tokenResponse = flow.newTokenRequest(code)
+                        .setRedirectUri(GoogleConfig.REDIRECT_URI)
+                        .execute();
+                Oauth2 oauth2 = new Oauth2.Builder(
+                        GoogleNetHttpTransport.newTrustedTransport(),
+                        JacksonFactory.getDefaultInstance(),
+                        flow.createAndStoreCredential(tokenResponse, null)
+                ).setApplicationName("SMAACVS").build();
+                Userinfo googleUserinfo = oauth2.userinfo().get().execute();
+
+                Usuario usuario = new Usuario();
+                usuario.setNombreDeUsuario(googleUserinfo.getName());
+                usuario.setEmail(googleUserinfo.getEmail());
+                usuario.setFoto(googleUserinfo.getPicture());
+
+                ctx.sessionAttribute("user", usuario);
+                ctx.redirect("/");
+            } catch (IOException | GeneralSecurityException e) {
+                e.printStackTrace();
+                ctx.redirect("/login");
+            }
+        } else {
+            ctx.redirect("/login");
+        }
     }
 }
